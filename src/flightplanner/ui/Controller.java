9ce2bcb023a0bb38d3f40226b00805122172e9f2
Jon Gunnar Hannesson <jgh060@gmail.com>
2021-04-10 13:56:24 +0000

    controller

--- src/flightplanner/ui/Controller.java
+++ src/flightplanner/ui/Controller.java
@@ -1,4 +1,28 @@
 package flightplanner.ui;
 
+import flightplanner.controllers.FlightSearchController;
+import flightplanner.data.FlDataConnection;
+import flightplanner.entities.Airport;
+
+import java.util.ArrayList;
+
 public class Controller {
+    FlightSearchController searchController;
+    FlDataConnection connection;
+    public Controller(){
+        connection = new FlDataConnection();
+        searchController = FlightSearchController.getInstance();
+        searchController.setConnection(connection);
+    }
+
+    public ArrayList<Airport> getAirports(){
+        ArrayList<Airport> airports = null;
+        try {
+            airports = searchController.searchAirports();
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        return airports;
+    }
 }
