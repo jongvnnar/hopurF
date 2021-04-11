ee59f6660c6a7fb07cd2aa8f6fa6307ed105f93b
sigruneva <sev12@hi.is>
2021-04-11 21:27:05 +0000

    Byrjun á næsta scene-i

--- src/flightplanner/ui/Controller.java
+++ src/flightplanner/ui/Controller.java
@@ -4,7 +4,10 @@ import flightplanner.controllers.FlightSearchController;
 import flightplanner.data.FlDataConnection;
 import flightplanner.entities.Airport;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
 import java.net.URL;
@@ -66,3 +69,22 @@ public class Controller implements Initializable {
 
 }
 
+
+    /**
+     * When this method is called, it will change the scene to "Bókunarstaðfesting"
+     * <p>
+     * oookidoki hér er fullt af villum en þegar þetta fæst til að virka þá á þetta
+     * að virka þannig að þegar maður keyrir og ýtir á "Bóka" þá poppar nýr scenebuilder
+     * gluggi upp með næsta scene-i
+     * - SEV
+     */
+    public void changeScreenButtonPushed(ActionEvent event) {
+        Parent bookParent = FXMLLoader.load(getClass().getResource("ClickBoka.fxml"));
+        Scene clickBokaScene = new Scene(clickBokaParent);
+
+        //This line gets the Stage Information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(clickBokaScene);
+        window.show();
+    }
