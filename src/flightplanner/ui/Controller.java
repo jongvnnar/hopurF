4f3b940dc063ce0ee3652cec9e2b4cef316a43bf
stein <steinunngretak@gmail.com>
2021-04-12 14:25:03 +0000

    sgk

--- src/flightplanner/ui/Controller.java
+++ src/flightplanner/ui/Controller.java
@@ -4,13 +4,15 @@ import flightplanner.controllers.FlightSearchController;
 import flightplanner.data.FlDataConnection;
 import flightplanner.entities.Airport;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 
 public class Controller implements Initializable {
     @FXML
@@ -66,27 +68,21 @@ public class Controller implements Initializable {
     }
 
 
-
-
-    /*
+    /**
      * When this method is called, it will change the scene to "Bókunarstaðfesting"
+     * <p>
      * oookidoki hér er fullt af villum en þegar þetta fæst til að virka þá á þetta
      * að virka þannig að þegar maður keyrir og ýtir á "Bóka" þá poppar nýr scenebuilder
      * gluggi upp með næsta scene-i
      * - SEV
      */
+    public void changeScreenButtonPushed(ActionEvent event) {
+        Parent bookParent = FXMLLoader.load(getClass().getResource("ClickBoka.fxml"));
+        Scene clickBokaScene = new Scene(clickBokaParent);
 
+        //This line gets the Stage Information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-    //public void changeScreenButtonPushed(ActionEvent event) {
-    //Parent bookParent = FXMLLoader.load(getClass().getResource("ClickBoka.fxml"));
-    //Scene clickBokaScene = new Scene(clickBokaParent);
-
-    //This line gets the Stage Information
-    //Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-    //window.setScene(clickBokaScene);
-    //window.show();
-    //}
-}
-
-
+        window.setScene(clickBokaScene);
+        window.show();
+    }
