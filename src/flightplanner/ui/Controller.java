4d00f6c0171e8e6d40b0696fd3517f1ab5d753c4
stein <steinunngretak@gmail.com>
2021-04-12 09:59:58 +0000

    sgk

--- src/flightplanner/ui/Controller.java
+++ src/flightplanner/ui/Controller.java
@@ -3,88 +3,27 @@ package flightplanner.ui;
 import flightplanner.controllers.FlightSearchController;
 import flightplanner.data.FlDataConnection;
 import flightplanner.entities.Airport;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.collections.ObservableList;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-    @FXML
-    private Button bookButton;
-
-    @FXML
-    private ComboBox departureCombo;
-
-    @FXML
-    private ComboBox arrivalCombo;
-
-    @FXML
-    private DatePicker pickdateDatePicker;
-
-    @FXML
-    private ListView flightListView;
-
-    @FXML
-    private TextField firstnameTextField;
-    @FXML
-    private TextField lastnameTextField;
-
-    @FXML
-    private TextField ssTextField;
+public class Controller {
     FlightSearchController searchController;
     FlDataConnection connection;
-
-    public Controller() {
+    public Controller(){
         connection = new FlDataConnection();
         searchController = FlightSearchController.getInstance();
         searchController.setConnection(connection);
     }
 
-    public ArrayList<Airport> getAirports() {
+    public ArrayList<Airport> getAirports(){
         ArrayList<Airport> airports = null;
         try {
             airports = searchController.searchAirports();
-        } catch (Exception e) {
+        }
+        catch(Exception e){
             System.err.println(e.getMessage());
         }
         return airports;
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        departureCombo.getItems().removeAll(departureCombo.getItems());
-        departureCombo.getItems().addAll("Reykjavík", "Akureyri", "Egilsstaðir", "Ísafjörður", "Keflavík");
-        departureCombo.getSelectionModel().select("Veldu brottfararstað");
-
-        arrivalCombo.getItems().removeAll(arrivalCombo.getItems());
-        arrivalCombo.getItems().addAll("Reykjavík", "Akureyri", "Egilsstaðir", "Ísafjörður", "Keflavík");
-        arrivalCombo.getSelectionModel().select("Veldu brottfararstað");
-    }
-
 }
-
-
-    /**
-     * When this method is called, it will change the scene to "Bókunarstaðfesting"
-     * <p>
-     * oookidoki hér er fullt af villum en þegar þetta fæst til að virka þá á þetta
-     * að virka þannig að þegar maður keyrir og ýtir á "Bóka" þá poppar nýr scenebuilder
-     * gluggi upp með næsta scene-i
-     * - SEV
-     */
-    public void changeScreenButtonPushed(ActionEvent event) {
-        Parent bookParent = FXMLLoader.load(getClass().getResource("ClickBoka.fxml"));
-        Scene clickBokaScene = new Scene(clickBokaParent);
-
-        //This line gets the Stage Information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        window.setScene(clickBokaScene);
-        window.show();
-    }
