192b951c01d8e7bd5b5a2da961ac5d9cfa626af6
stein <steinunngretak@gmail.com>
2021-04-13 16:24:40 +0000

    login

--- src/flightplanner/ui/loginController.java
+++ src/flightplanner/ui/loginController.java
@@ -1,4 +1,33 @@
 package flightplanner.ui;
 
-public class loginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+
+
+    public void changeBookButtonPushed(ActionEvent event) throws IOException {
+        Parent loginParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene clickloginScene = new Scene(loginParent);
+
+        //This line gets the Stage Information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(clickloginScene);
+        window.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
